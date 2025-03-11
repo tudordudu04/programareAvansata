65941184e33939bcd74d73f782209eff2c6b50b2
Caprian Tudor <tudordudu04@gmail.com>
2025-03-11 23:07:01 +0200

    bonus works now

--- lab01/src/bonus.java
+++ lab01/src/bonus.java
@@ -1,31 +1,35 @@
 public class bonus {
-    public static void printMatrix(int[][] matrix) {
+    public static void printMatrix(int[][] matrix, int[] nr) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
-            System.out.println();
+
+            System.out.println("nr vecici: " + nr[i]);
         }
     }
+
     public static boolean areAdjacent(int[] x, int[][] adjMatrix) {
-        for(int i = 0; i < x.length; i++)
-            for(int j = 0; j < x.length; j++)
-                if(adjMatrix[x[i]][x[j]] != 1 && x[i]!=x[j])
+        for (int i = 0; i < x.length; i++)
+            for (int j = 0; j < x.length; j++) {
+                int aux = adjMatrix[x[i]][x[j]];
+                if (aux != 1 && x[i] != x[j])
                     return false;
+            }
         return true;
     }
-    ///OK verifica sa nu se repete vecinii in bkt
+
     public static boolean OK(int[] x, int k){
         for(int i = 0 ; i < k ; ++ i)
             if(x[k] == x[i])
                 return false;
         return true;
     }
-    public static boolean genCombinations(int initialSize, int cliqueSize, int[] adjacent, int[][] matrix, int[] x)
+    public static boolean genCombinations(int initialSize, int cliqueSize, int[] neighbours, int[][] matrix, int[] x)
     {
-        for(int i = 0 ; i < adjacent.length ; ++ i)
+        for(int i = 0; i < neighbours.length ; ++ i)
         {
-            x[initialSize] = adjacent[i];
+            x[initialSize] = neighbours[i];
             if(OK(x, initialSize))
                 if(initialSize == cliqueSize - 1) {
                     if(areAdjacent(x, matrix)){
@@ -36,37 +40,37 @@ public class bonus {
                     }
                 }
                 else
-                    return genCombinations(initialSize+1, cliqueSize, adjacent, matrix, x);
+                    if(genCombinations(initialSize+1, cliqueSize, neighbours, matrix, x))
+                        return true;
         }
+
         return false;
     }
-    public static boolean isClique(int x, int cliqueSize, int[][] matrix) {
-        int j, nr = 0;
-        for(j = 0; j<matrix[x].length; j++)
-            if(matrix[x][j] == 1)
+
+    public static boolean isClique(int row, int cliqueSize, int[][] matrix) {
+        int column, nr = 0;
+        for(column = 0; column < matrix[row].length; column++)
+            if(matrix[row][column] == 1)
                 nr++;
         nr ++;
         int[] neighbours = new int[nr];
-        for(j = matrix[x].length-1; j>=0; j--)
-            if(matrix[x][j] == 1 || x == j){
-                neighbours[nr-1] = j;
+        for(column = matrix[row].length-1; column >=0; column--)
+            if(matrix[row][column] == 1 || row == column){
+                neighbours[nr-1] = column;
                 nr--;
             }
-//        for(j = 0; j<neighbours.length; j++)
-//            System.out.print(neighbours[j] + " ");
-//        System.out.println();
         int[] verifyArray = new int[cliqueSize];
         if(genCombinations(0, cliqueSize, neighbours, matrix, verifyArray))
             return true;
         return false;
     }
+
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Didn't provide exactly one argument");
             System.exit(-1);
         }
         int cliqueSize = Integer.parseInt(args[0]);
-
         int nrNodes;
         do {
             nrNodes = (int) (Math.random() * 100);
@@ -83,26 +87,26 @@ public class bonus {
             }
         }
 
-        printMatrix(adjMatrix);
 
-        int[] nrAdjecentNodes = new int[nrNodes];
+        int[] nrAdjacentNodes = new int[nrNodes];
         for (i = 0; i < nrNodes; i++) {
-            nrAdjecentNodes[i] = 0;
+            nrAdjacentNodes[i] = 0;
             for (j = 0; j < nrNodes; j++) {
-                if (adjMatrix[i][j] == 1)
-                    nrAdjecentNodes[i]++;
+                if (adjMatrix[i][j] == 1) {
+                    nrAdjacentNodes[i]++;
+                }
             }
         }
+        printMatrix(adjMatrix, nrAdjacentNodes);
 
         for (i = 0; i < nrNodes; i++) {
-            if(nrAdjecentNodes[i] >= cliqueSize - 1){
+            if(nrAdjacentNodes[i] >= cliqueSize - 1){
                 if(isClique(i, cliqueSize, adjMatrix)) {
                     System.out.println("A clique has been found.");
                     break;
                 }
             }
         }
-
-
     }
 }
+
